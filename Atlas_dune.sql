7635b2c166ef6955187db781f60e4794aa80ec16
P-Christopher <150606448+P-Christopher@users.noreply.github.com>
2024-08-25 19:58:57 -0400

    Update Atlas_dune.sql
    Join price table to the solana token address for ATLAS

--- Atlas_dune.sql
+++ Atlas_dune.sql
@@ -1,10 +1,15 @@
+
 select
   pt.token_id,
   pt.blockchain,
   pt.symbol,
   pt.contract_address,
-  pt.decimals
+  pt.decimals, 
+  ul.price,
+  ul.minute
 from
   prices.tokens as pt
+  left join prices.usd_latest ul
+  on pt.contract_address = ul.contract_address
 where
-  pt.token_id = 'atlas-star-atlas-wormhole'
+  pt.token_id = 'atlas-star-atlas-wormhole';
